dd3a1978d50d75327e5a8b7ea2ea91610840fd78
kibiruki <+>
2024-07-16 04:29:51 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Write your solution in this file!
+// index.js
+
+// Initialize an empty employee object
+const employee = {};
+
+// Function to update employee with key and value non-destructively
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+  return {
+    ...employee,
+    [key]: value
+  };
+}
+
+// Function to update employee with key and value destructively
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+  employee[key] = value;
+  return employee;
+}
+
+// Function to delete key from employee non-destructively
+function deleteFromEmployeeByKey(employee, key) {
+  const newEmployee = { ...employee };
+  delete newEmployee[key];
+  return newEmployee;
+}
+
+// Function to delete key from employee destructively
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+  delete employee[key];
+  return employee;
+}
+
+module.exports = {
+  employee,
+  updateEmployeeWithKeyAndValue,
+  destructivelyUpdateEmployeeWithKeyAndValue,
+  deleteFromEmployeeByKey,
+  destructivelyDeleteFromEmployeeByKey
+};
